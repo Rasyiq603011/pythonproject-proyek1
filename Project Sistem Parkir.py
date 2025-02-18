7059c821b6d2bb9b1123d6e6fff45809c2b0ab25
Rasyiq03 <mns.rasyiq03@gmail.com>
2025-02-18 11:00:54 +0700

    membuat project

---
+++ Project Sistem Parkir.py
@@ -0,0 +1,12 @@
+# Nama File : Project Sistem Parkir.property
+# tanggal : 18 Februari 2025
+# Pembuat : Cipta Haidar, Muhammad Arravi, Muhammad Nabil, Diax Putri
+
+
+# input DeprecationWarning
+
+# hitung biaya
+
+# display
+
+# safe data
