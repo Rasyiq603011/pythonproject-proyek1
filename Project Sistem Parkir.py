9cb64c6f226af4a957b60353f00e6dcf92651e98
Rasyiq03 <mns.rasyiq03@gmail.com>
2025-02-18 11:51:30 +0700

    Merubah project

--- Project Sistem Parkir.py
+++ Project Sistem Parkir.py
@@ -1,12 +1,39 @@
 # Nama File : Project Sistem Parkir.property
 # tanggal : 18 Februari 2025
-# Pembuat : Cipta Haidar, Muhammad Arravi, Muhammad Nabil, Diax Putri
+# Pembuat : Cipta Haidar, Muhammad Arravi, Muhammad Nabil, Diaz Putri
 
 
-# input DeprecationWarning
+# input data 
+# def input():
+#     cek = True
+#     while(cek):
+#         Jenis = input("Masukan Jenis Kendaraan (Mobil atau Motor) : ")
+#         Masuk = input("Waktu Masuk : ")        
+#         Keluar = input("Waktu Keluar : ")
+
+Tipe = input("Masukan Jenis Kendaraan (Mobil atau Motor) : ")
+lama = input("Lama Masuk : ")
+platnomor = input("Masukan plat nomor : ")
 
 # hitung biaya
 
 # display
 
 # safe data
+def savedata(TipeKendaraan, lamaMasuk, nomorpolisi, harga):
+    f = open("Data parkir.txt","a")
+    f.write(
+            "Data Parkir\n"
+            "Tipe Kendaraan : " + TipeKendaraan +
+            "\nNomor Polisi   : " + nomorpolisi +
+            "\nLama Parkir    : " + lamaMasuk  +
+            "\nBiaya parkir   : " + harga +
+            "\n\n"
+            )
+    f.close()
+
+savedata(Tipe, lama, platnomor, "12.000" )
+
+f = open("Data parkir.txt","r")
+print(f.read())
+f.close()
